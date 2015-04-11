65b440eaf8216bedba4e01e4c4c11a8c600c29b6
Tim Kenny <timkenny@umich.edu>
2015-04-11 08:52:41 -0400

    Full App Selection and storage functionality

---
+++ NOtify/app/src/main/java/me/dontnotify/notify/DatabaseAccess.java
@@ -0,0 +1,195 @@
+package me.dontnotify.notify;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by timkenny on 4/10/2015.
+ */
+public class DatabaseAccess {
+
+    private DatabaseAccessHelper dbHelper;
+    private Integer curr_index;
+
+
+    protected DatabaseAccess(Context context) {
+        dbHelper = new DatabaseAccessHelper(context);
+        curr_index = -1;
+    }
+
+    private void insertRow(int index,
+                          String appName,
+                          String packageName,
+                          String action,
+                          String addText) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues vals = new ContentValues();
+        vals.put(DatabaseEntry.COLUMN_NAME_INDEX, index);
+        vals.put(DatabaseEntry.COLUMN_NAME_APP, appName);
+        vals.put(DatabaseEntry.COLUMN_NAME_PACKAGE, packageName);
+        vals.put(DatabaseEntry.COLUMN_NAME_ACTION, action);
+        vals.put(DatabaseEntry.COLUMN_NAME_ADD, addText);
+
+        db.insert(DatabaseEntry.TABLE_NAME,
+                DatabaseEntry.COLUMN_NAME_NULLABLE,
+                vals);
+    }
+
+    public void updateRows(ArrayList<MainActivity.NotifyItem> list){
+        MainActivity.NotifyItem item;
+        if( curr_index + 1 >= list.size()) return;
+        int i = curr_index+1;
+        for(; i < list.size(); i++){
+            item = list.get(i);
+            insertRow(i, item.getName(), item.getPackageName(),
+                    item.getAction(), item.getAdd());
+        }
+        curr_index = i-1;
+    }
+
+    public MainActivity.NotifyItem getRow (int index) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                DatabaseEntry.COLUMN_NAME_APP,
+                DatabaseEntry.COLUMN_NAME_PACKAGE,
+                DatabaseEntry.COLUMN_NAME_ACTION,
+                DatabaseEntry.COLUMN_NAME_ADD
+        };
+        String where = DatabaseEntry.COLUMN_NAME_INDEX +
+                " = ?";
+        String[] whereArgs = new String[] { Integer.toString(index) };
+
+        Cursor c = db.query( DatabaseEntry.TABLE_NAME,
+                            projection, where, whereArgs,
+                            null, null, null);
+        if ( c.getCount() > 1 )
+            throw new RuntimeException("More than one SQL row with same index");
+
+        c.moveToFirst();
+        MainActivity.AppInfo appInfo = new MainActivity.AppInfo(
+                c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_APP)),
+                c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_PACKAGE))
+        );
+        MainActivity.NotifyItem notifyItem = new MainActivity.NotifyItem(
+                appInfo,
+                c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_ACTION)),
+                c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_ADD))
+        );
+
+        c.close();
+        return notifyItem;
+    }
+
+    public ArrayList<MainActivity.NotifyItem> getAllData(){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                DatabaseEntry.COLUMN_NAME_INDEX,
+                DatabaseEntry.COLUMN_NAME_APP,
+                DatabaseEntry.COLUMN_NAME_PACKAGE,
+                DatabaseEntry.COLUMN_NAME_ACTION,
+                DatabaseEntry.COLUMN_NAME_ADD
+        };
+        String orderBy = DatabaseEntry.COLUMN_NAME_INDEX +
+                        " ASC";
+
+        Cursor c = db.query( DatabaseEntry.TABLE_NAME,
+                projection, null, null,
+                null, null, orderBy);
+
+        c.moveToFirst();
+        Integer preIndex = -1, index = -1;
+        MainActivity.NotifyItem[] retArray =
+                new MainActivity.NotifyItem[c.getCount()];
+
+        while( !c.isAfterLast() ) {
+            index = c.getInt(
+                    c.getColumnIndexOrThrow(
+                            DatabaseEntry.COLUMN_NAME_INDEX));
+            if(preIndex == index) {
+                throw new RuntimeException("More than 1 entry per Index");
+            }
+            preIndex = index;
+
+            MainActivity.AppInfo appInfo = new MainActivity.AppInfo(
+                    c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_APP)),
+                    c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_PACKAGE))
+            );
+            MainActivity.NotifyItem notifyItem = new MainActivity.NotifyItem(
+                    appInfo,
+                    c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_ACTION)),
+                    c.getString(c.getColumnIndexOrThrow(DatabaseEntry.COLUMN_NAME_ADD))
+            );
+
+            retArray[index] = notifyItem;
+            c.moveToNext();
+        }
+        curr_index = index;
+        c.close();
+
+        ArrayList<MainActivity.NotifyItem> retList = new ArrayList<MainActivity.NotifyItem>();
+        for(int i = curr_index; i >= 0; i--){
+            retList.add(0,retArray[i]);
+        }
+        return retList;
+    }
+
+    /* Inner class that defines the table contents */
+    public static abstract class DatabaseEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_INDEX = "listIndex";
+        public static final String COLUMN_NAME_APP = "appName";
+        public static final String COLUMN_NAME_PACKAGE = "packageName";
+        public static final String COLUMN_NAME_ACTION = "action";
+        public static final String COLUMN_NAME_ADD = "addString";
+        public static final String COLUMN_NAME_NULLABLE = "nullable";
+    }
+
+    public class DatabaseAccessHelper extends SQLiteOpenHelper {
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "FeedReader.db";
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String INT_TYPE = " INTEGER";
+        private static final String COMMA_SEP = ",";
+        private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + DatabaseEntry.TABLE_NAME + " (" +
+                DatabaseEntry._ID + " INTEGER PRIMARY KEY," +
+                DatabaseEntry.COLUMN_NAME_INDEX + INT_TYPE + COMMA_SEP +
+                DatabaseEntry.COLUMN_NAME_APP + TEXT_TYPE + COMMA_SEP +
+                DatabaseEntry.COLUMN_NAME_PACKAGE + TEXT_TYPE + COMMA_SEP +
+                DatabaseEntry.COLUMN_NAME_ACTION + TEXT_TYPE + COMMA_SEP +
+                DatabaseEntry.COLUMN_NAME_ADD + TEXT_TYPE + COMMA_SEP +
+                DatabaseEntry.COLUMN_NAME_NULLABLE + TEXT_TYPE +
+            " )";
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + DatabaseEntry.TABLE_NAME;
+
+        public DatabaseAccessHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+
+    }
+}
--- NOtify/app/src/main/java/me/dontnotify/notify/MainActivity.java
+++ NOtify/app/src/main/java/me/dontnotify/notify/MainActivity.java
@@ -1,19 +1,27 @@
 package me.dontnotify.notify;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,9 +29,25 @@ public class MainActivity extends ActionBarActivity {
     private EditText inputField;
     private Button addButton;
     private ListView itemList;
+    private NoDefaultSpinner selectApp;
+    private NoDefaultSpinner selectAction;
+
+    private ArrayList<AppInfo> AppInfoArray;
+    public static class AppInfo {
+        public AppInfo(String startAppName,
+                       String startPackageName) {
+            appName = startAppName;
+            packageName = startPackageName;
+        }
+        public String appName; //Display Name
+        public String packageName; //Unique Identifier!
+    }
+
 
     private ArrayList<NotifyItem> items;
     private MyArrayAdapter adapter;
+
+    private DatabaseAccess db;
 
 
     @Override
@@ -34,28 +58,65 @@ public class MainActivity extends ActionBarActivity {
         inputField = (EditText) findViewById(R.id.inputText);
         addButton = (Button) findViewById(R.id.addButton);
         itemList = (ListView) findViewById(R.id.itemList);
+        selectApp = (NoDefaultSpinner)
+                findViewById(R.id.selectAppSpinner);
+        selectAction = (NoDefaultSpinner)
+                findViewById(R.id.selectActionSpinner);
+        AppInfoArray = new ArrayList<AppInfo>();
 
         items = new ArrayList<NotifyItem>();
         adapter = new MyArrayAdapter(this, items);
-
         itemList.setAdapter(adapter);
 
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String input = inputField.getText().toString();
+        db = new DatabaseAccess(this);
 
-                items.add(new NotifyItem(input));
-
-                adapter.notifyDataSetChanged();
-            }
-        });
-
+        fillSelectApp();
+        restoreDatafromDisk();
     }
 
-    public void onToggleClicked(View view) {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        storeDataToDisk();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fillSelectApp();
+    }
+
+    public void onAddClicked(View view) {
+        String input = inputField.getText().toString();
+        if(selectApp.getSelectedItemPosition() == AdapterView.INVALID_POSITION){
+            Toast toast = Toast.makeText(
+                    this,"Please Select Application",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        AppInfo appIn = AppInfoArray.get(selectApp.getSelectedItemPosition());
+        if(selectAction.getSelectedItem() == null) {
+            Toast toast = Toast.makeText(
+                    this,
+                    "Please Select Action",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        String actionIn = selectAction.getSelectedItem().toString();
+        items.add(new NotifyItem(appIn, actionIn, input));
+        adapter.notifyDataSetChanged();
+    }
+
+    public void onAllowClicked(View view) {
         // Is the toggle on?
-        boolean on = ((ToggleButton) view).isChecked();
+        boolean on = ((Switch) view).isChecked();
 
         if (on) {
             //Allow
@@ -63,37 +124,37 @@ public class MainActivity extends ActionBarActivity {
             // Ignore
         }
     }
+
     //
     // NotifyItem
     //
     // Container class for information about a scavenger hunt item.
     //
-    private class NotifyItem {
-        private String name;
-        private ToggleButton tog;
-//        private Bitmap icon;
+    public static class NotifyItem {
+        private Switch allowSwitch;
+        private AppInfo appInfo;
+        private String actionInfo;
+        private String addInfo;
 
-        public NotifyItem(String n){
-            name = n;
-            tog = null;
+        public NotifyItem(AppInfo startApp, String startAction,
+                          String startAdd){
+            appInfo = startApp;
+            actionInfo = startAction;
+            addInfo = startAdd;
+            allowSwitch = null;
         }
 
-        public NotifyItem(String n, ToggleButton b){
-            name = n;
-            tog = b;
-        }
-
-        public void setTB(ToggleButton b){
-            tog = b;
-        }
-
-        public ToggleButton getTB(){
-            return tog;
+        public void setSW(Switch s){ allowSwitch = s;  }
+        public boolean getSWvalue(){
+            return allowSwitch.isChecked();
         }
 
         public String getName(){
-            return name;
+            return appInfo.appName;
         }
+        public String getPackageName() { return appInfo.packageName; }
+        public String getAction() { return actionInfo; }
+        public String getAdd() { return addInfo; }
     } // NotifyItem
 
     //
@@ -121,18 +182,51 @@ public class MainActivity extends ActionBarActivity {
 
             // Initialize UI for row
             TextView name = (TextView) row.findViewById(R.id.rowText);
-//            ImageView pict = (ImageView) row.findViewById(R.id.rowImage);
-            ToggleButton tb = (ToggleButton) row.findViewById(R.id.togglebutton);
+            Switch sw = (Switch) row.findViewById(R.id.allowSwitch);
 
             // Assign UI data
-            name.setText(values.get(pos).getName());
-
-//            if (values.get(pos).getIcon() != null){
-//                pict.setImageBitmap(values.get(pos).getIcon());
-//            }
+            values.get(pos).setSW(sw);
+            name.setText( values.get(pos).getAction() + " from " +
+                            values.get(pos).getName() );
 
             return row;
         }
     } // MyArrayAdapter
+
+    // add items into spinner dynamically
+    public void fillSelectApp() {
+        List<String> list = new ArrayList<String>();
+        AppInfoArray.clear();
+
+        final PackageManager pm = getPackageManager();
+        //get a list of installed apps.
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            String app_name = (String) pm.getApplicationLabel(packageInfo);
+            list.add(app_name);
+            AppInfoArray.add(new AppInfo(app_name, packageInfo.packageName));
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectApp.setAdapter(dataAdapter);
+    }
+
+    //Make Data Permanent to SQLite
+    public void storeDataToDisk() {
+        db.updateRows(items);
+    }
+
+    //Restore Data from SQLite
+    public void restoreDatafromDisk() {
+        ArrayList<NotifyItem> updatedItems = db.getAllData();
+        if( updatedItems.size() != items.size() ) {
+            items = updatedItems;
+            adapter = new MyArrayAdapter(this, items);
+            itemList.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
 
 }
